d52b1b79a99da6c74a76f3ec35d793e5cfcd65d1
Igor <gaes1k@yandex.ru>
2025-03-08 01:38:01 +0300

    recursive logic try

--- src/main/java/org/example/HorseClass.java
+++ src/main/java/org/example/HorseClass.java
@@ -1,24 +1,49 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class HorseClass {
     public int getStepsCount(int n, String[] boardString) {
         char[][] board = new char[n][n];
+        int[][] kingStepped = new int[n][n];
+        int[][] horseStepped = new int[n][n];
+        Coordinate currCoordinate =new Coordinate();
 
         for (int i = 0; i < n; i++) {
             board[i] = boardString[i].toCharArray();
+            Arrays.fill(horseStepped[i], -1);
+            Arrays.fill(kingStepped[i], -1);
+            if(boardString[i].contains("S")){
+                int column=boardString[i].indexOf('S');
+                horseStepped[i][column]=0;
+                currCoordinate.setRow(i);
+                currCoordinate.setColumn(column);
+            }
         }
-        List<StepInfo> stepInfoList = new ArrayList<>();
+
+        int stepNum=0;
+        boolean isKing=false;
+
 
 
         return 3;
     }
 
-    public int getStepCountFromThisCell(boolean isKingNow, int column, int row) {
+    public int getStepCountFromThisCell(boolean isKingNow, Coordinate coordinate, int[][] kingStepped, int[][]horseStepped) {
+        Queue<Coordinate> kingVariants=new LinkedList<>();
+        Queue<Coordinate> horseVariants=new LinkedList<>();
+
         return 0;
     }
 
 
+    public List<Coordinate> getNextStepsVariantsForKing(Coordinate coordinate, int[][] kingStepped){
+
+        return null;
+    }
+
+    public List<Coordinate> getNextStepsVariantsForHorse(Coordinate coordinate, int[][] horseStepped){
+
+        return null;
+    }
 }
